449c9b7f4acc874347c09f6dceccca2400cd20b8
Maooookai <1220568032@qq.com>
2022-05-05 15:23:34 +0800

    add coin

--- service/coin.py
+++ service/coin.py
@@ -15,20 +15,45 @@ def coin_have(message: qqbot.Message):
 def random_add_coin(message: qqbot.Message):
     message_reference = MessageReference()
     message_reference.message_id = message.id
-    salt = random.randint(1, 4)
+    salt = random.randint(1, 8)
+    reason = '发言随机金币'
     if salt == 1:
         coin = random.randint(1, 3)
-        add_coin(message.author.id, coin, '发言随机金币')
+        add_coin(message.author.id, coin, reason)
         return qqbot.MessageSendRequest(f"感觉你说的很有道理，送你{coin}金币！", message.id, message_reference=message_reference)
     elif salt == 2:
         coin = random.randint(1, 3)
-        add_coin(message.author.id, coin, '发言随机金币')
+        add_coin(message.author.id, coin, reason)
         return qqbot.MessageSendRequest(f"你这话真有意思，给你{coin}金币！", message.id, message_reference=message_reference)
     elif salt == 3:
         coin = random.randint(1, 3)
-        add_coin(message.author.id, coin, '发言随机金币')
+        add_coin(message.author.id, coin, reason)
         return qqbot.MessageSendRequest(f"好，这是{coin}金币，请收下", message.id, message_reference=message_reference)
     elif salt == 4:
         coin = random.randint(1, 3)
-        add_coin(message.author.id, coin, '发言随机金币')
+        add_coin(message.author.id, coin, reason)
         return qqbot.MessageSendRequest(f"笑死，给你{coin}金币", message.id, message_reference=message_reference)
+    elif salt == 5:
+        coin = random.randint(1, 3)
+        add_coin(message.author.id, coin, reason)
+        return qqbot.MessageSendRequest(f"他真的，我哭死，给你{coin}金币", message.id, message_reference=message_reference)
+    elif salt == 6:
+        coin = random.randint(1, 3)
+        add_coin(message.author.id, coin, reason)
+        return qqbot.MessageSendRequest(f"这都行? 给你{coin}金币", message.id, message_reference=message_reference)
+    elif salt == 7:
+        coin = random.randint(1, 3)
+        add_coin(message.author.id, coin, reason)
+        return qqbot.MessageSendRequest(f"怎会如此，请收下{coin}金币", message.id, message_reference=message_reference)
+    elif salt == 8:
+        coin = random.randint(1, 3)
+        add_coin(message.author.id, coin, reason)
+        return qqbot.MessageSendRequest(f"就这? 拿下这{coin}金币吧", message.id, message_reference=message_reference)
+    elif salt == 9:
+        coin = random.randint(1, 3)
+        add_coin(message.author.id, coin, reason)
+        return qqbot.MessageSendRequest(f"你真可爱! 我要给你{coin}金币!", message.id, message_reference=message_reference)
+    elif salt == 10:
+        coin = random.randint(1, 3)
+        add_coin(message.author.id, coin, reason)
+        return qqbot.MessageSendRequest(f"你看天上那朵云，像不像我现在送你的{coin}金币?", message.id, message_reference=message_reference)
