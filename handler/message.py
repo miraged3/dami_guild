cf70342c3e5690787eb129a52e0b8e7e6b5d3fcb
Maooookai <1220568032@qq.com>
2022-05-12 20:54:07 +0800

    change divine chance

--- handler/message.py
+++ handler/message.py
@@ -38,7 +38,7 @@ async def message_handler(event, message: qqbot.Message):
             if random.randint(1, 100) < 3:
                 qqbot.logger.info('触发人工智障: ' + message.content)
                 await msg_api.post_message(message.channel_id, ma_reply(message))
-        if datetime.today().weekday() == 3 and random.randint(1, 100) < 2 and message.channel_id == '1356661':
+        if datetime.today().weekday() == 3 and random.randint(1, 1000) < 8 and message.channel_id == '1356661':
             qqbot.logger.info('触发疯狂星期四: ' + message.author.username)
             await msg_api.post_message(message.channel_id, random_kfc_notice(message))
     elif hasattr(message, 'attachments'):
