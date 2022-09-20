2247235e100bed911c3711b23afca05069838488
Maooookai <1220568032@qq.com>
2022-09-20 14:57:11 +0800

    bug fix

--- service/stupid.py
+++ service/stupid.py
@@ -11,7 +11,7 @@ def ma_reply(message: qqbot.Message):
 def repeat(message: qqbot.Message):
     reply_words = ['真的吗', '就是啊', '不会吧', '笑死', '就这啊', '怎么会这样', '+1', '饿了', '绝了', '好困', '哈人']
     i = random.randint(1, 100)
-    if i > 95:
+    if i < 95:
         return qqbot.MessageSendRequest(message.content, message.id)
     else:
         return qqbot.MessageSendRequest(reply_words[random.randint(0, len(reply_words) - 1)], message.id)
