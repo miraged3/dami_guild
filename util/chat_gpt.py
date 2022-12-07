ee686c632ccc801e2d1a0268dce88d18f55212a2
Maooookai <1220568032@qq.com>
2022-12-07 12:55:20 +0800

    bug fix

--- util/chat_gpt.py
+++ util/chat_gpt.py
@@ -1,12 +1,16 @@
 import os
 
+import qqbot
 from pychatgpt import Chat
+from qqbot import MessageReference
 from qqbot.core.util.yaml_util import YamlUtil
 
 config = YamlUtil.read(os.path.join(os.path.dirname(__file__), "../config.yaml"))
 
 
-def ask_gpt(message: str) -> str:
+def ask_gpt(message: qqbot.Message):
+    message_reference = MessageReference()
+    message_reference.message_id = message.id
     chat = Chat(email=config["chatgpt"]["email"], password=config["chatgpt"]["password"],
                 proxies="http://localhost:7890")
-    return chat.ask(message)
+    return qqbot.MessageSendRequest(chat.ask(message), message.id, message_reference=message_reference)
